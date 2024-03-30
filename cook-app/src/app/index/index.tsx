a533e6b5ca2ef6c9203afac85de755593086b1ea
Savio Nascimento <savionascimentodev@gmail.com>
2024-03-30 19:08:26 -0400

    ✨ updating project

--- cook-app/src/app/index/index.tsx
+++ cook-app/src/app/index/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native"
+import { styles } from "./styles"
 
 export default function Index() {
   return (
-    <View>
-      <Text>React Native na Prática</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha os produtos</Text>
+      <Text style={styles.subtitle}>
+        Descubra receitas baseadas nos produtos que você escolheu.
+      </Text>
     </View>
   )
 }
